26685f6dcaea35ac0616b6e65241c0d2e0551027
Obianuju <enekebe123@gmail.com>
2024-11-13 13:09:27 -0500

    Initial Commit

---
+++ ApplePieTests/ApplePieTests.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePieTests.swift
+//  ApplePieTests
+//
+//  Created by Obianuju Enekebe on 11/13/24.
+//
+
+import Testing
+@testable import ApplePie
+
+struct ApplePieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
